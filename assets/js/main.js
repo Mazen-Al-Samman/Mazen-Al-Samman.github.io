de5299a15d988c6c19a0d5ddcebff04e8ddd1349
Mazen Alsamman <mazen.a@opensooq.com>
2021-12-06 00:07:08 +0200

    Push to domain

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
+    var numberOfDivs = 3;
     if ($(window).width() > 975) {
         $('.collapse').addClass('show');
     } else {
+        numberOfDivs = 1;
         $('.collapse').removeClass('show');
     }
+    $('.testimonial_wrapper').slick({
+        infinite: true,
+        slidesToShow: numberOfDivs,
+        centerMode: true,
+        slidesToScroll: 1,
+        dots: true,
+        lazyLoad: 'ondemand',
+      });
 });
 
 $(window).resize(function() {
     if ($(window).width() > 975) {
+        numberOfDivs = 3;
         $('.collapse').addClass('show');
     } else {
+        numberOfDivs = 1;
         $('.collapse').removeClass('show');
     }
 });
@@ -22,3 +34,17 @@ $('[data-bs-target="#navbarNav"]').on("click", function(){
         $("#main-content").removeClass("mt-200");
     }
 });
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
